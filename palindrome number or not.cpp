2acb8a55736a67f6ace66fd11088a518eac0ed64
Vedant <91217411+VedantCode04@users.noreply.github.com>
2024-06-21 00:49:20 +0530

    Update palindrome number or not.cpp

--- palindrome number or not.cpp
+++ palindrome number or not.cpp
@@ -2,13 +2,28 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
-        string s = to_string(x);
-        int n = s.size();
-        for(int i = 0; i < n/2; i++){
-            if(s[i] != s[n-i-1]) return false;
+        int temp = x;
+        long long ans = 0;
+        while(temp > 0){
+            int rem = temp % 10;
+            ans = ans*10 + rem;
+            temp = temp/10;
         }
 
-        return true;
+        if(ans == x) return true;
+        return false;
     }
 };
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+        
+//         string s = to_string(x);
+//         int n = s.size();
+//         for(int i = 0; i < n/2; i++){
+//             if(s[i] != s[n-i-1]) return false;
+//         }
+
+//         return true;
+//     }
+// };
