22892ae5451f402ac1cf55e86c22cb6d82995d70
Vedant <91217411+VedantCode04@users.noreply.github.com>
2024-07-27 19:17:28 +0530

    Create check if two array strings are equivalent.cpp

---
+++ check if two array strings are equivalent.cpp
@@ -0,0 +1,16 @@
+//lc 1662
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string str1 = "", str2 = "";
+        for(auto str: word1){
+            str1 += str;
+        }
+        for(auto str: word2){
+            str2 += str;
+        }
+        if(str1 == str2) return true;
+        return false;
+    }
+};
