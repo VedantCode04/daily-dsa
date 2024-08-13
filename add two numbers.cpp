937d51eb56adc98abbe3827dce6c72bd7480ef6a
Vedant <91217411+VedantCode04@users.noreply.github.com>
2024-08-13 23:40:46 +0530

    Update add two numbers.cpp

--- add two numbers.cpp
+++ add two numbers.cpp
@@ -16,8 +16,11 @@ public:
         ListNode* newHead = new ListNode(-1), *curr = newHead;
         ListNode* t1 = l1, *t2 = l2;
         int carry = 0 ;
-        while(t1 != NULL && t2 != NULL){
-            long long sum = long(t1->val) + long(t2->val);
+        while(t1 != NULL || t2 != NULL){
+            long data1 = 0, data2 = 0;
+            if(t1 != NULL) data1 = t1->val;
+            if(t2 != NULL) data2 = t2->val;
+            long long sum = data1 + data2;
             sum = sum + carry;
             if(sum < 10){
                 ListNode* temp = new ListNode(sum);
@@ -31,44 +34,8 @@ public:
                 curr = temp;
             }
 
-            t1 = t1->next;
-            t2 = t2->next;
-        }
-
-        while(t1 != NULL){
-            long long sum = long(t1->val) + carry;
-
-            if(sum < 10){
-                ListNode* temp = new ListNode(sum);
-                curr->next = temp;
-                curr = temp;
-                carry = 0;
-            }else {
-                carry = 1;
-                ListNode* temp = new ListNode(sum%10);
-                curr->next = temp;
-                curr = temp;
-            }
-
-            t1 = t1->next;
-        }
-
-        while(t2 != NULL){
-            long long sum = long(t2->val) + carry;
-
-            if(sum < 10){
-                ListNode* temp = new ListNode(sum);
-                curr->next = temp;
-                curr = temp;
-                carry = 0;
-            }else {
-                carry = 1;
-                ListNode* temp = new ListNode(sum%10);
-                curr->next = temp;
-                curr = temp;
-            }
-
-            t2 = t2->next;
+            if(t1 != NULL) t1 = t1->next;
+            if(t2 != NULL) t2 = t2->next;
         }
 
         if(carry == 1){
